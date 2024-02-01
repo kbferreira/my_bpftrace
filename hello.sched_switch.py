eb9a3ecfd0146c218900a04b35b4c738fddb2d81
Kurt B. Ferreira <kbferre@sandia.gov>
2024-02-01 11:01:14 -0700

    more typos

--- hello.sched_switch.py
+++ hello.sched_switch.py
@@ -29,7 +29,7 @@ struct data_t {
 
 int hello(void *ctx) {
    struct data_t *data; 
-   struct user_msg_t *p;
+   //struct user_msg_t *p;
    char message[12] = "Hello World";
    u64 uid;
    u64 *p;
